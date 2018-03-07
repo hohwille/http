1620c34a1ac7800cfdf2840e6d947b57643035c4
Joerg Hohwiller <hohwille@users.sourceforge.net>
2018-03-07 21:36:07 +0100

    moved from util

---
+++ src/main/java/net/sf/mmm/http/api/entity/HttpEntity.java
@@ -0,0 +1,16 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package net.sf.mmm.http.api.entity;
+
+import net.sf.mmm.http.api.HttpMessage;
+import net.sf.mmm.util.lang.api.StringWritable;
+
+/**
+ * This is the interface for an entity as optional part of an {@link HttpMessage} (body).
+ *
+ * @author hohwille
+ * @since 1.0.0
+ */
+public interface HttpEntity extends StringWritable {
+
+}
---
+++ src/main/java/net/sf/mmm/http/api/upnp/package-info.java
@@ -0,0 +1,9 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * TODO: this class ...
+ *
+ * @author hohwille
+ * @since 1.0.0
+ */
+package net.sf.mmm.http.api.upnp;
